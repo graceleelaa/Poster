0607016e5ed4bd9b792b5406fa5a2ae268008444
graceleelaa <graceleelaa@Graces-Mac.local>
2017-03-09 14:53:43 -0800

    s

--- Poster.js
+++ Poster.js
@@ -1,132 +1,220 @@
-//creates the balls and sets their position as well as the random timer for each
-private void spawnBalls1() {
-
-    Rectangle ball1 = new Rectangle();
-    ball1.x = MathUtils.random(639, 641);
-    ball1.y = 720;
-    ball1.width = 32;
-    ball1.height = 32;
-    balls1.add(ball1);
-    lastDropTime = TimeUtils.nanoTime();
-
-}
-
-private void spawnBalls2() {
-
-    Rectangle ball2 = new Rectangle();
-    ball2.x = 0;
-    ball2.y = MathUtils.random(359, 361);
-    ball2.width = 32;
-    ball2.height = 32;
-    balls2.add(ball2);
-    lastDropTime = TimeUtils.nanoTime();
-}
-
-private void spawnBalls3() {
-
-    Rectangle ball3 = new Rectangle();
-    ball3.x = MathUtils.random(639, 641);
-    ball3.y = 0;
-    ball3.width = 32;
-    ball3.height = 32;
-    balls3.add(ball3);
-    lastDropTime = TimeUtils.nanoTime();
-}
-
-private void spawnBalls4() {
-
-    Rectangle ball4 = new Rectangle();
-    ball4.x = 1280;
-    ball4.y = MathUtils.random(359, 361);
-    ball4.width = 32;
-    ball4.height = 32;
-    balls4.add(ball4);
-    lastDropTime = TimeUtils.nanoTime();
-}
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Rectangle;
 
 
-//draws the balls
-    for (Rectangle ball1 : balls1) {
+/*
+ * Ball class which manages the balls position and motion during the game
+ */
 
-        batch.draw(Ball, ball1.x, ball1.y);
+public class Ball {
+
+    private Point pos, startPos;
+    private int dx, dy;
+    private int xrangemin, xrangemax, yrangemin, yrangemax; //was private
+    private int size;
+    public boolean inPlay;
+    private Color color;
+    
+    BallSmokeParticle b;
+    
+    /*
+     * Creates a ball at specified position with specified colour
+     */
+    public Ball(Point p, Color c) {
+        this.pos = p;
+        this.startPos = new Point(pos.x, pos.y);
+        this.size = 20;
+        this.dx = 1;
+        this.dy = 1;
+        this.inPlay = false;
+        this.color = c;
     }
-
-    for (Rectangle ball2 : balls2) {
-
-        batch.draw(Ball, ball2.x, ball2.y);
+    
+    public Point getPos() {
+        return pos;
     }
-
-    for (Rectangle ball3 : balls3) {
-
-        batch.draw(Ball, ball3.x, ball3.y);
+    
+    public void startPlay() {
+        // If game already in progress cannot start new game
+        if(inPlay)
+            return;
+        // Otherwise initialise new game round (keeps old scores)
+        inPlay = true;
+        
+        // Make random start movement
+        dx = (Math.random()<0.5) ? -1 : 1; 
+        dy = (Math.random()<0.5) ? -1 : 1;
+        pos = new Point(startPos.x, startPos.y); //?? this mat not work-> new Point(startPos.x, startPos.y) ??
     }
-
-    for (Rectangle ball4 : balls4) {
-
-        batch.draw(Ball, ball4.x, ball4.y);
+    
+    public Point getVelocity() {
+        Point p = new Point(dx, dy);
+        return p;
     }
-
-
-// if the time minus the time of the last ball spawn is less than x then spawn another ball in a random place
-    if (TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnBalls1();
-
-    Iterator<Rectangle> iter1 = balls1.iterator();
-    while(iter1.hasNext()) {
-
-        Rectangle balls1 = iter1.next();
-        balls1.y -= 500 * Gdx.graphics.getDeltaTime();
-        if (balls1.overlaps(square)) {
-
-            score++;
-            showScore = "Score: " + score;
-            iter1.remove();
+    
+    public int getSize() {
+        return size;
+    }
+    
+    /*
+     * Returns position where the computerised paddle should be depending on game orientation
+     */
+    public int getPaddlePos(boolean orientation) {
+        if (orientation)
+            return pos.y;
+        else
+            return pos.x;
+    }
+    
+    /*
+     * Manages how the ball bounces on player paddles
+     */
+    public boolean bounce(Player p, Game game) {
+        int var;
+        if(game.orientation) 
+            var = p.getVarY();
+        else
+            var = p.getVarX();
+        
+        int paddleLen = p.getLength();
+        
+        // If ball position is either side of paddle -> no bounce
+        if (game.orientation) {
+            if (pos.y < var || pos.y >= var+paddleLen)
+                return false;
         }
-    }
-
-    // if the time minus the time of the last ball spawn is less than x then spawn another ball in a random place
-    if (TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnBalls2();
-
-    Iterator<Rectangle> iter2 = balls2.iterator();
-    while (iter2.hasNext()) {
-
-        Rectangle balls2 = iter2.next();
-        balls2.x += 500 * Gdx.graphics.getDeltaTime();
-        if (balls2.overlaps(square)) {
-
-            score++;
-            showScore = "Score: " + score;
-            iter2.remove();
+        else if (pos.x < var || pos.x >= var+paddleLen)
+            return false;
+        
+        boolean bounced = false;
+        Rectangle paddleRect = p.getRect();
+        
+        // Test for ball hitting side of paddle
+        Rectangle YballRect = new Rectangle(pos.x-size/2, pos.y-size/2, size, size);
+        YballRect.translate(dx, 0);
+        if(paddleRect.intersects(YballRect)) {
+            dx = -dx;
+            bounced = true;
         }
-    }
 
-    // if the time minus the time of the last ball spawn is less than x then spawn another ball in a random place
-    if (TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnBalls3();
-
-    Iterator<Rectangle> iter3 = balls3.iterator();
-    while(iter3.hasNext()) {
-
-        Rectangle balls3 = iter3.next();
-        balls3.y += 500 * Gdx.graphics.getDeltaTime();
-        if (balls3.overlaps(square)) {
-
-            score++;
-            showScore = "Score: " + score;
-            iter3.remove();
+        // Test for ball hitting top/front of paddle
+        Rectangle XballRect = new Rectangle(pos.x-size/2, pos.y-size/2, size, size);
+        XballRect.translate(dx, dy);
+        if(paddleRect.intersects(XballRect)) {
+            dy = -dy;
+            bounced = true;
         }
+        return bounced;
     }
-
-    // if the time minus the time of the last ball spawn is less than x then spawn another ball in a random place
-    if (TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnBalls4();
-
-    Iterator<Rectangle> iter4 = balls4.iterator();
-    while(iter4.hasNext()) {
-
-        Rectangle balls4 = iter4.next();
-        balls4.x -= 500 * Gdx.graphics.getDeltaTime();
-        if (balls4.overlaps(square)) {
-
-            score++;
-            showScore = "Score: " + score;
-            iter4.remove();
+    
+    
+    public void move(Game game) {
+        // If game not being played cannot move
+        if(!inPlay)
+            return;
+        // Move ball
+        pos.x += dx;
+        pos.y += dy;
+        
+        // Check that ball is within playing area and adjust, also monitor when points scored, also check orientation of game
+        if(pos.y < yrangemin) {
+            if (game.orientation) {
+                // Bounce off top wall
+                pos.y = yrangemin;
+                dy = -dy;
+            }
+            else {
+                // End this game and increment bottom players score
+                inPlay = false;
+                SoundManager.YOULOSE.play();
+                game.rplayer.incScore(); 
+                if (game.deathMatch)
+                    game.lplayer.setLength(game.lplayer.getLength()-20);
+                game.state = Game.WAITING;
+            }
         }
+        
+        if(pos.y > yrangemax) {
+            if (game.orientation) {
+                // Bounce off bottom wall
+                pos.y = yrangemax;
+                dy = -dy;
+            }
+            else {
+                // End this game and increment top players score
+                inPlay = false;
+                SoundManager.YOULOSE.play();
+                game.lplayer.incScore(); 
+                if (game.deathMatch)
+                    game.rplayer.setLength(game.rplayer.getLength()-20);
+                game.state = Game.WAITING;
+            }
+        }
+        
+        if(pos.x < xrangemin) {
+            if (game.orientation) {
+                // End this game and increment right players score
+                inPlay = false;
+                SoundManager.YOULOSE.play();
+                game.rplayer.incScore();
+                if (game.deathMatch)
+                    game.lplayer.setLength(game.lplayer.getLength()-20);
+                game.state = Game.WAITING;
+            }
+            else {
+                // Bounce off left wall
+                pos.x = xrangemin;
+                dx = -dx;
+            }
+        }
+        
+        if(pos.x > xrangemax) {
+            if (game.orientation) {
+                // End this game and increment left players score
+                inPlay = false;
+                SoundManager.YOULOSE.play();
+                game.lplayer.incScore();  
+                if (game.deathMatch)
+                    game.rplayer.setLength(game.rplayer.getLength()-20);
+                game.state = Game.WAITING;
+            }
+            else {
+                // Bounce off right wall
+                pos.x = xrangemax;
+                dx = -dx;
+                }
+            }
+        }
+        
+    /*
+     * Sets walls with ball is to be contained within
+     */
+    public void setRange(int xmin, int xmax, int ymin, int ymax) {
+        xrangemin = xmin+size/2;
+        xrangemax = xmax-size/2;
+        yrangemin = ymin+size/2;
+        yrangemax = ymax-size/2;
     }
+    
+    /*
+     * Change ball colour while game is paused then change colour back when game is resumed
+     */
+    public void flash() {
+        if (this.color == Color.YELLOW)
+            this.color = Color.RED;
+        else
+            this.color = Color.YELLOW;
+    }
+    
+    /*
+     * Draws the ball to screen
+     */
+    public void draw(Graphics g) {
+        if(!inPlay)
+            return;
+        g.setColor(color);
+        g.fillOval(pos.x-size/2, pos.y-size/2, size, size);
+    }
+}
